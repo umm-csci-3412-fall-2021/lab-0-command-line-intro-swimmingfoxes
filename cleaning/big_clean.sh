3409da9bc108bad427e3d1d33dc60ad1b0931b7d
Isabelle Hjelden <hjeld013@sentinel.morris.umn.edu>
2021-09-02 17:11:39 -0500

    remove unnecessary directory in cleaned compressed tgz file

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,4 +2,7 @@ NAME=$1
 TEMPNAME=$(mktemp -d "temp.XXXXXXXXXXXXXXX")
 tar -xzf $NAME -C $TEMPNAME
 grep -rl "DELETE ME!" "$TEMPNAME" | xargs rm
-tar -zcf "cleaned_$NAME" $TEMPNAME
+OLDPATH=$(pwd)
+cd $TEMPNAME
+tar -zcf "cleaned_$NAME" "$($NAME | cut -f 1 -d '.')"
+mv "cleaned_$NAME" $OLDPATH
