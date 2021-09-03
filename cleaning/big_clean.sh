82eef4250c678ed4768edc1ff1ac12bb431a82b4
Isabelle Hjelden <hjeld013@sentinel.morris.umn.edu>
2021-09-03 11:37:14 -0500

    Finishe implementing big_clean.sh and add necessary comments, and clean up code.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,27 @@
-NAME=$1
-TEMPNAME=$(mktemp -d "temp.XXXXXXXXXXXXXXX")
-tar -xzf $NAME -C $TEMPNAME
-grep -rl "DELETE ME!" "$TEMPNAME" | xargs rm
+#!/bin/bash
+
+# Accept argument and store it into variable "FILENAME"
+FILENAME=$1
+
+# Make a temporary directory and save the name of said directory 
+# into the TEMPFOLDER variable
+TEMPFOLDER=$(mktemp -d "temp.XXXXXXXXXXXXXXX")
+
+# Untar and decompress the tar file passed into the program
+tar -xzf "$FILENAME" -C "$TEMPFOLDER"
+
+# Look through the files and look for ones that have 
+# a "DELETE ME!" string in them.
+grep -rl "DELETE ME!" "$TEMPFOLDER" | xargs rm
+
+# Save current directory path to be used later
 OLDPATH=$(pwd)
-cd $TEMPNAME
-tar -zcf "cleaned_$NAME" "$($NAME | cut -f 1 -d '.')"
-mv "cleaned_$NAME" $OLDPATH
+
+# Go into the temporary folder, if that fails, then exit the program.
+cd "$TEMPFOLDER" || exit
+
+# Compress the files in the directory
+tar -zcf "cleaned_$FILENAME" "$(basename "$FILENAME" .tgz)"
+
+# Move newly created tar archive to the original directory
+mv "cleaned_$FILENAME" "$OLDPATH"
